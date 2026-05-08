a7f03fbf1b2bdc3f32edc944d4e1b665e8b0ba17
mediatool34-collab <mediatool34@gmail.com>
2026-05-09 00:45:39 +0300

    Create meta.js

---
+++ api/auth/connect/meta.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+    const { uid } = req.query;
+    const appId = process.env.META_CLIENT_ID || '1984316142437697';
+    const appUrl = process.env.APP_URL || 'https://anty-ads-operator.vercel.app';
+    const redirectUri = `${appUrl}/api/auth/callback/meta`;
+
+  const fbUrl = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${uid || ''}&scope=ads_management,ads_read,business_management,pages_read_engagement,pages_show_list`;
+
+  res.redirect(fbUrl);
+}
